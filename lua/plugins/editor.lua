0ad861724ddfde821790450d51b4d8536ef9c5ba
Dylan Deyal <dylandd13@outlook.com>
2025-03-28 12:43:14 -0400

    lsp for lua

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -220,6 +220,38 @@ return {
                         end
                     })
                 end,
+                lus_ls = function(_, opts)
+                    vim.api.nvim_create_autocmd("LspAttach", {
+                        callback = function(args)
+                            if client.workspace_folders then
+                                local path = client.workspace_folders[1].name
+                                if path ~= vim.fn.stdpath('config') and (vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc')) then
+                                    return
+                                end
+                            end
+
+                            client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+                                runtime = {
+                                    -- Tell the language server which version of Lua you're using
+                                    -- (most likely LuaJIT in the case of Neovim)
+                                    version = 'LuaJIT'
+                                },
+                                -- Make the server aware of Neovim runtime files
+                                workspace = {
+                                    checkThirdParty = false,
+                                    library = {
+                                        vim.env.VIMRUNTIME
+                                        -- Depending on the usage, you might want to add additional paths here.
+                                        -- "${3rd}/luv/library"
+                                        -- "${3rd}/busted/library",
+                                    }
+                                    -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
+                                    -- library = vim.api.nvim_get_runtime_file("", true)
+                                }
+                            })
+                        end
+                    })
+                end,
             },
         },
         config = function(_, opts)
@@ -436,5 +468,12 @@ return {
             })
             vim.keymap.set('n', '<Leader>tt', ':lua require("neotest").run.run()<CR>', { desc = 'Run test' })
         end,
-    }
+    },
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        config = true
+        -- use opts = {} for passing setup options
+        -- this is equivalent to setup({}) function
+    },
 }
